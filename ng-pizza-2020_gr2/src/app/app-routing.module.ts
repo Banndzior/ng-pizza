5f7bb13a2c74139fa40f087a14112f48b607b2e0
kamil mijacz <kamil.mijacz@softwarehut.com>
2020-06-06 15:30:48 +0200

    TODO fix

--- ng-pizza-2020_gr2/src/app/app-routing.module.ts
+++ ng-pizza-2020_gr2/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// TODO 4: Stworz routing dla listy pizz: pizza component
-// TODO 7: Stworz routing dla pizzy po jej id/nazwie
+// TODO 7: Stworz routing dla listy pizz: pizza component
+// TODO 7+: Stworz routing dla pizzy po jej id/nazwie
 // TODO 10: Stworz routing dla listy pizz z parametrem nazwa/fragment nazwy
 
 const routes: Routes = [];
