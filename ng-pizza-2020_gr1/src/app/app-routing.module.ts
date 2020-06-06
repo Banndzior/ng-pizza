11fde973aaa4e69d28e0fbc254ef4d4a8363a13d
kamil mijacz <kamil.mijacz@softwarehut.com>
2020-06-06 13:58:04 +0200

    TODO 7: Routing

--- ng-pizza-2020_gr1/src/app/app-routing.module.ts
+++ ng-pizza-2020_gr1/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PizzaComponent } from './pizza/pizza.component';
 
 // Przyklad: https://github.com/Banndzior/ng-routing/blob/master/ng-routing_gr1/src/app/app-routing.module.ts
 // TODO 7: Stworz routing dla listy pizz: pizza component: /pizza
 // TODO 7+: Stworz routing dla pizzy po jej id/nazwie: /pizza/search/marg
 // TODO 10: Stworz routing dla listy pizz z parametrem nazwa/fragment nazwy: /pizza/:id
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pizza', pathMatch: 'full' },
+  { path: 'pizza', component: PizzaComponent },
+  { path: '**', redirectTo: 'pizza' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
