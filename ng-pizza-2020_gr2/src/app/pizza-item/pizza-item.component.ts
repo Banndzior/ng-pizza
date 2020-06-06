30962a03de83cd06372b788a5aab53da86cfdd83
kamil mijacz <kamil.mijacz@softwarehut.com>
2020-06-06 17:14:12 +0200

    TODO 7+

--- ng-pizza-2020_gr2/src/app/pizza-item/pizza-item.component.ts
+++ ng-pizza-2020_gr2/src/app/pizza-item/pizza-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PizzaService } from '../pizza.service';
+import { Pizza } from '../pizza';
 
 @Component({
   selector: 'app-pizza-item',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza-item.component.css']
 })
 export class PizzaItemComponent implements OnInit {
+  pizzaId: string;
+  pizza: Pizza;
 
-  constructor() { }
+  constructor(route: ActivatedRoute, private pizzaSvc: PizzaService) {
+    this.pizzaId = route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit() {
+    this.pizzaSvc.getPizza(parseInt(this.pizzaId, 10)).subscribe((response) => {
+      this.pizza = response;
+    });
   }
 
 }
