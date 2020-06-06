fb29318937fb6772f06286b7096b149ebfea736c
kamil mijacz <kamil.mijacz@softwarehut.com>
2020-06-06 15:49:42 +0200

    Przyklad routingu

--- ng-pizza-2020_gr2/src/app/app-routing.module.ts
+++ ng-pizza-2020_gr2/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 // TODO 7: Stworz routing dla listy pizz: pizza component
 // TODO 7+: Stworz routing dla pizzy po jej id/nazwie
+// https://github.com/Banndzior/ng-routing/blob/master/ng-routing_gr2/src/app/app-routing.module.ts
 // TODO 10: Stworz routing dla listy pizz z parametrem nazwa/fragment nazwy
 
 const routes: Routes = [];
