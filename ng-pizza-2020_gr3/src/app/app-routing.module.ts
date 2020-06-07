aa5f3f177ac1ec23041adb40ea3826d345b40ca1
kamil mijacz <kamil.mijacz@softwarehut.com>
2020-06-07 14:46:42 +0200

    Delete fix

--- ng-pizza-2020_gr3/src/app/app-routing.module.ts
+++ ng-pizza-2020_gr3/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { PizzaComponent } from "./pizza/pizza.component";
-import { PizzaItemComponent } from "./pizza-item/pizza-item.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PizzaComponent } from './pizza/pizza.component';
+import { PizzaItemComponent } from './pizza-item/pizza-item.component';
 
 // TODO 6: Stworz routing dla listy pizz: pizza component (/pizza)
 // TODO 6+: Stworz routing dla pizzy po jej id/nazwie (/pizza/:id)
@@ -9,10 +9,10 @@ import { PizzaItemComponent } from "./pizza-item/pizza-item.component";
 // TODO 10: Stworz routing dla listy pizz z parametrem nazwa/fragment nazwy
 
 const routes: Routes = [
-  { path: "", redirectTo: "pizza", pathMatch: "full" },
-  { path: "pizza", component: PizzaComponent },
-  { path: "pizza/:id", component: PizzaItemComponent },
-  { path: "**", redirectTo: "pizza" },
+  { path: '', redirectTo: 'pizza', pathMatch: 'full' },
+  { path: 'pizza', component: PizzaComponent },
+  { path: 'pizza/:id', component: PizzaItemComponent },
+  { path: '**', redirectTo: 'pizza' },
 ];
 
 @NgModule({
