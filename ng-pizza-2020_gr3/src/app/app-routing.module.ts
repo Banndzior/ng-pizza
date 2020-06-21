a5775675132b195b8da5e7b587ef0fbca7a69787
kamil mijacz <kamil.mijacz@softwarehut.com>
2020-06-21 15:07:33 +0200

    Uwagi do TODO

--- ng-pizza-2020_gr3/src/app/app-routing.module.ts
+++ ng-pizza-2020_gr3/src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { PizzaItemComponent } from './pizza-item/pizza-item.component';
 // TODO 6: Stworz routing dla listy pizz: pizza component (/pizza)
 // TODO 6+: Stworz routing dla pizzy po jej id/nazwie (/pizza/:id)
 // https://github.com/Banndzior/ng-routing/blob/master/ng-routing_gr2/src/app/app-routing.module.ts
-// TODO 10: Stworz routing dla listy pizz z parametrem nazwa/fragment nazwy
+// TODO 10: Stworz routing dla listy pizz z parametrem nazwa/fragment nazwy (pizza/search/:name)
 
 const routes: Routes = [
   { path: '', redirectTo: 'pizza', pathMatch: 'full' },
   { path: 'pizza', component: PizzaComponent },
+  { path: 'pizza/search/:name', component: PizzaComponent },
   { path: 'pizza/:id', component: PizzaItemComponent },
   { path: '**', redirectTo: 'pizza' }
 ];
