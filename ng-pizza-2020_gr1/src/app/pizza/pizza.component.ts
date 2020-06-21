08b878fa81bd3af4b5e232dcc398cb65f6760678
Piotr Dyrda <piotr.dyrda88@gmail.com>
2020-06-21 18:22:37 +0200

    fix routing

--- ng-pizza-2020_gr1/src/app/pizza/pizza.component.ts
+++ ng-pizza-2020_gr1/src/app/pizza/pizza.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { PizzaService } from "../pizza.service";
 import { Pizza } from "../pizza";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-pizza",
@@ -9,17 +10,25 @@ import { Pizza } from "../pizza";
 })
 export class PizzaComponent implements OnInit {
   pizzas: Pizza[];
+  name: string;
 
-  constructor(private pizzaSvc: PizzaService) {}
+  constructor(private route: ActivatedRoute, private pizzaSvc: PizzaService) {}
 
   ngOnInit() {
+    this.name = this.route.snapshot.paramMap.get("name");
     this.getPizza();
     this.pizzaSvc.onPizzaChange.subscribe(() => this.getPizza());
   }
   getPizza() {
     this.pizzaSvc.getPizzas().subscribe((response) => {
-      console.log(response);
-      this.pizzas = response.value;
+      if (this.name) {
+        this.pizzas = response.value;
+        this.pizzas = this.pizzas.filter((pizza) =>
+          pizza.name.includes(this.name)
+        );
+      } else {
+        this.pizzas = response.value;
+      }
     });
   }
 }
