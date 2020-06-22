ccc0a84a5979155e0167f4190e03381cb31f8ff0
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-06-22 14:16:07 +0200

    material ui navbar added

--- ng-pizza-2020_gr3/src/app/navbar/navbar.component.ts
+++ ng-pizza-2020_gr3/src/app/navbar/navbar.component.ts
@@ -1,23 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-navbar",
   template: `
-    <nav class="nav-bar">
-      <div class="container">
-        <button [routerLink]="['pizza/edit']">Stwórz pizzę</button>
-      </div>
-    </nav>
+    <mat-toolbar color="primary">
+      <button mat-raised-button color="accent" [routerLink]="['pizza/edit']">
+        Add NEW pizza
+      </button>
+    </mat-toolbar>
   `,
-  styles: [
-    `
-      .nav-bar {
-        width: 100%;
-        height: 80px;
-        background: red;
-      }
-    `,
-  ],
 })
 export class NavbarComponent implements OnInit {
   constructor() {}
