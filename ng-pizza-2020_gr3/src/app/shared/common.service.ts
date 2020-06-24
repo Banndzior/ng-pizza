9bbd6b23b2f20e264d91545b8cf86a3073f68e7a
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-06-24 12:40:22 +0200

    options buttons added

--- ng-pizza-2020_gr3/src/app/shared/common.service.ts
+++ ng-pizza-2020_gr3/src/app/shared/common.service.ts
@@ -8,10 +8,17 @@ export class CommonService {
   constructor() {}
 
   private navBtnStatus = new BehaviorSubject<boolean>(false);
-
   actualBtnStatus = this.navBtnStatus.asObservable();
 
   changeBtnStatus(status: boolean) {
     this.navBtnStatus.next(status);
+    this.changeOptionsStatus(false);
+  }
+
+  private optionsStatus = new BehaviorSubject<boolean>(false);
+  actualOptionsStatus = this.optionsStatus.asObservable();
+
+  changeOptionsStatus(status: boolean) {
+    this.optionsStatus.next(status);
   }
 }
