c9b984dafb7c5f97d47eadace6ab32d80029cfb2
lulek92 <lulmichal@gmail.com>
2020-06-24 09:26:42 +0200

    styled editor and pizza card

--- ng-pizza-2020_gr2/src/app/app.module.ts
+++ ng-pizza-2020_gr2/src/app/app.module.ts
@@ -9,6 +9,14 @@ import { PizzaItemComponent } from "./pizza-item/pizza-item.component";
 import { PizzaEditorComponent } from "./pizza-editor/pizza-editor.component";
 import { NgxPaginationModule } from "ngx-pagination";
 import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { MatCardModule } from "@angular/material/card";
 
 // TODO 1: Dodaj komponent PizzaItem za pomoca Angular CLI: ng g c pizza-item
 // TODO 2: Dodaj komponent PizzaEditor za pomoca Angular CLI
@@ -26,6 +34,13 @@ import { FormsModule } from "@angular/forms";
     HttpClientModule,
     FormsModule,
     NgxPaginationModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
