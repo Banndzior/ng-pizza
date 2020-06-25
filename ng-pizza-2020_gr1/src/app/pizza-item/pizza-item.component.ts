cb2d290da9b3b15415ba97d919755c29d2aed1b1
dante74 <dan_te@wp.pl>
2020-06-25 15:15:07 +0200

    kolorki

--- ng-pizza-2020_gr1/src/app/pizza-item/pizza-item.component.ts
+++ ng-pizza-2020_gr1/src/app/pizza-item/pizza-item.component.ts
@@ -1,19 +1,27 @@
-import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { Pizza } from "../pizza";
-import { PizzaService } from "../pizza.service";
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pizza } from '../pizza';
+import { PizzaService } from '../pizza.service';
 
 @Component({
-  selector: "app-pizza-item",
-  templateUrl: "./pizza-item.component.html",
-  styleUrls: ["./pizza-item.component.css"],
+  selector: 'app-pizza-item',
+  templateUrl: './pizza-item.component.html',
+  styleUrls: ['./pizza-item.component.css'],
 })
 export class PizzaItemComponent implements OnInit {
+  isVisible = false;
+  newImage = '';
+  onFocus = 'card';
+
   @Input()
   pizza: Pizza;
 
+
   @Output()
   removePizza = new EventEmitter();
+
+  @Output()
+  changePizza = new EventEmitter();
 
   constructor(private route: ActivatedRoute, private service: PizzaService) { }
 
@@ -22,11 +30,41 @@ export class PizzaItemComponent implements OnInit {
     // id && this.service.getPizza(id).subscribe((resp) => (this.pizza = resp));
   }
 
+  typeImage(event) {
+    this.newImage = event.target.value;
+    console.log(this.newImage);
+  }
   remove() {
     this.removePizza.emit(this.pizza);
+    console.log('aaaaaaaaaaa');
   }
 
   modify() {
-    console.log(this.service.modifyPizza(this.pizza));
+    this.isVisible = !this.isVisible;
+  }
+
+  change() {
+    console.log('change');
+    this.pizza.photoUrl = this.newImage;
+    this.newImage = '';
+    console.log(this.pizza);
+    this.changePizza.emit(this.pizza);
+  }
+
+  changeFocusIn() {
+    if (this.onFocus === 'downFocus') {
+      this.onFocus = 'downFocus';
+    } else this.onFocus = 'onFocus';
+
+  }
+  changeFocusOut() {
+    if (this.onFocus === 'downFocus') {
+      this.onFocus = 'downFocus';
+    } else this.onFocus = 'card';
+
+  }
+
+  changeFocusDown() {
+    (this.onFocus === 'downFocus') ? this.onFocus = 'onFocus' : this.onFocus = 'downFocus';
   }
 }
--- ng-pizza-2020_gr1/src/app/pizza/pizza.component.ts
+++ ng-pizza-2020_gr1/src/app/pizza/pizza.component.ts
@@ -13,36 +13,44 @@ export class PizzaComponent implements OnInit {
   pizzas: Pizza[];
   name: string;
 
-  constructor( private pizzaSvc: PizzaService, private route: ActivatedRoute) { }
+  constructor(private pizzaSvc: PizzaService, private route: ActivatedRoute) { }
 
 
   ngOnInit() {
     this.name = this.route.snapshot.paramMap.get("name");
     this.getPizza();
-    this.pizzaSvc.onChange.subscribe(()=>{
+    this.pizzaSvc.onChange.subscribe(() => {
       this.getPizza();
-      
+
     })
   }
 
-  getPizza(){
+  getPizza() {
     this.pizzaSvc.getPizzas().subscribe(response => {
-      
-      if(this.name){
+      if (this.name) {
         this.pizzas = response.value;
-        this.pizzas = this.pizzas.filter( pizza => pizza.name.includes(this.name));
+        this.pizzas = this.pizzas.filter(pizza => pizza.name.includes(this.name));
       } else {
+        console.log('response = ' + response.value[0].name)
         this.pizzas = response.value;
       }
     });
   }
 
-  addPizza(pizza: Pizza) {
-    this.pizzaSvc.addPizza(pizza).subscribe(() => this.ngOnInit());
-  }
+  // addPizza(pizza: Pizza) {
+  //   this.pizzaSvc.addPizza(pizza).subscribe(() => this.ngOnInit());
+  // }
 
   removePizza(pizza: Pizza) {
+    console.log('Remove' + pizza);
     this.pizzaSvc.removePizza(pizza).subscribe(() => {
+      this.ngOnInit();
+    });
+  }
+
+  changePizza(pizza: Pizza) {
+    console.log('Pizzzzzzzza : ' + pizza);
+    this.pizzaSvc.changePizza(pizza).subscribe(() => {
       this.ngOnInit();
     });
   }
